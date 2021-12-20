a791a58fb48601dd5e4c7073e26dc97d8beac18e
Nicolas Del Piano <ndel314@gmail.com>
2021-12-19 22:05:11 -0300

    Add complete lexer
    Parser in progress

---
+++ src/monkey/parser/parser_test.go
@@ -0,0 +1,15 @@
+package parser
+
+import (
+	"testing"
+
+	"monkey/ast"
+)
+
+func TestLetStatements(t *testing.T) {
+
+}
+
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+
+}
---
+++ src/monkey/token/token.go
@@ -0,0 +1,63 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	IF       = "if"
+	ELSE     = "else"
+	RETURN   = "return"
+	TRUE     = "true"
+	FALSE    = "false"
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	LT       = "<"
+	GT       = ">"
+	EQ       = "=="
+	NOT_EQ   = "!="
+)
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+	"true":   TRUE,
+	"false":  FALSE,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tt, ok := keywords[ident]; ok {
+		return tt
+	}
+	return IDENT
+}
