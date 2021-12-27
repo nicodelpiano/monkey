1497cf36deab6885fc473aee7ab8e57f8e1d66ba
Nicolas Del Piano <ndel314@gmail.com>
2021-12-26 23:07:33 -0300

    Add initial parser implementation

--- parser/parser.go
+++ parser/parser.go
@@ -27,6 +27,84 @@ func (p *Parser) nextToken() {
 	p.peekToken = p.l.NextToken()
 }
 
+func (p *Parser) parseIdentifier() *ast.Identifier {
+	// type Identifier struct {
+	// 	Token token.Token
+	// 	Value string
+	// }
+	if p.peekToken.Type != token.IDENT {
+		return nil
+	}
+	p.nextToken()
+	return &ast.Identifier{
+		Token: p.curToken,
+		Value: p.curToken.Literal,
+	}
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	}
+	return false
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+
+	// type LetStatement struct {
+	// 	Token token.Token
+	// 	Name  *Identifier
+	// 	Value Expression
+	// }
+	stmt := &ast.LetStatement{}
+	stmt.Token = p.curToken
+
+	identifier := p.parseIdentifier()
+	if identifier == nil {
+		return nil
+	}
+	stmt.Name = identifier
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
 }
